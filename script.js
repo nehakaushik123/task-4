09c4dda6c08dbf0f226fe2a004fb063821e10268
nehakaushik123 <51986313+nehakaushik123@users.noreply.github.com>
2021-01-25 16:26:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+(function () {
+
+
+var names = ["Dansih", "Guransh", "Pranita", "Amisha", "Deepakshi", "Hafsa", "Shahtaj", "Rabeeca", "Hussain", "Maaz"];
+
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+
+})();
